ada6b08ba34e0105f2c8d37126a9d857ec0f5772
abhay734 <vermaabhay734@gmail.com>
2025-06-08 00:48:25 +0530

    Navigation Properties In Entity Framework Core

--- NZWalks.API/Models/DTO/WalkDto.cs
+++ NZWalks.API/Models/DTO/WalkDto.cs
@@ -8,7 +8,7 @@
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
 
-        public Guid DifficultyId { get; set; }
-        public Guid RegionId { get; set; }
+        public RegionDto Region { get; set; }
+        public DifficultyDto Difficulty { get; set; }
     }
 }
--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f634197bfd1b3c61603e7444793c0017e12a572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+690bb328da6fdbdd7bdaf871913e41c33d1bb862")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
