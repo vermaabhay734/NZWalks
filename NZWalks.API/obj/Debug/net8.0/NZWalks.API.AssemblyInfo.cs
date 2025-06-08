841968b75f98278eb9f015b4e492db153f9197ba
abhay734 <vermaabhay734@gmail.com>
2025-06-09 00:17:12 +0530

    Add Filtering in Get all Method

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57141e3ff92a607a962af16aa92a92f8866514f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2ffabb49831e5ac730cfe8350ea825794712b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
